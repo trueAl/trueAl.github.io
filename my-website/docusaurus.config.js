0b598754a98f08d9a934c1268d80c0efa10af521
alexander.truemper <alexander@transcortex.dev>
2025-06-01 11:34:34 +0200

    new build

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'trueal', // Usually your GitHub org/user name.
   projectName: 'trueal.github.io', // Usually your repo name.
-  trailingSlash: false
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
